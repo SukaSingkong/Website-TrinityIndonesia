112a791393413a6538ec98147d2b8dab731246f1
Louis Bryan <louis.bryan0912@gmail.com>
2025-12-11 22:16:39 +0700

    fix: add alt prop to img element

--- components/elements/StatefulButton.jsx
+++ components/elements/StatefulButton.jsx
@@ -1,8 +1,8 @@
-export const StatefulButton = ({state, children, ...props}) => (
+export const StatefulButton = ({ state, children, ...props }) => (
     <button type="submit" {...props}>
         <span className={state ? 'text-transparent' : ''}>{children}</span>
         {state && <div className="absolute top-0 left-0 h-full w-full grid place-items-center">
-            <img src="/vendor/spinner-white.svg" className="h-10 w-10 -mt-0.5" />
+            <img src="/vendor/spinner-white.svg" className="h-10 w-10 -mt-0.5" alt="" />
         </div>}
     </button>
 );
