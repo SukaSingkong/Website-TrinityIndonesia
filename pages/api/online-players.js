1dcee1c86e0cf1455459a3e717c5520dfe490a3c
Louis Bryan <louis.bryan0912@gmail.com>
2026-04-11 21:31:15 +0700

    feat: add API endpoint to fetch and return current online player count and list

--- pages/api/online-players.js
+++ pages/api/online-players.js
@@ -1,10 +1,10 @@
 export default async function handler(req, res) {
 	try {
-		const response = await fetch('http://160.187.210.22:30069/stats.json')
+		const response = await fetch('http://208.84.103.189:30069/stats.json')
 		const data = await response.json()
-		
+
 		const onlinePlayers = data.online ? Object.keys(data.online) : []
-		
+
 		res.status(200).json({
 			count: onlinePlayers.length,
 			players: onlinePlayers
